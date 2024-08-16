16af8a625cbc61f90193253ebef36a5aa63f34f0
chanhwiim <chanhwiim459@gmail.com>
2024-08-16 17:37:43 +0900

    feat: add java script (post action)

---
+++ src/main/resources/static/post.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+    let userLoggedIn = /*[[${user != null}]]*/ false;
+
+    if (userLoggedIn) {
+        document.getElementById("Create").style.display = "inline";
+        document.getElementById("Edit").style.display = "inline";
+        document.getElementById("Delete").style.display = "inline";
+    } else {
+        document.getElementById("loginForm").style.display = "inline";
+    }
+
+    document.getElementById("Create").onclick = function () {
+        window.location.href = "/CreatePost";
+    }
+
+    document.getElementById("Edit").onclick = function () {
+        window.location.href = "/EditPost";
+    }
+
+    document.getElementById("Delete").onclick = function () {
+        window.location.href = "/deletePost";
+    }
+}
