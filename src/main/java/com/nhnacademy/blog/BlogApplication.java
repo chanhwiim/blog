ed6f0588501f2d23dac9eec239efda0b45e3ba27
chanhwiim <chanhwiim459@gmail.com>
2024-08-23 11:04:13 +0900

    feat: add basePackages

--- src/main/java/com/nhnacademy/blog/BlogApplication.java
+++ src/main/java/com/nhnacademy/blog/BlogApplication.java
@@ -2,8 +2,10 @@ package com.nhnacademy.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.nhnacademy.blog.domain")
 public class BlogApplication {
 
 	public static void main(String[] args) {
